70dc3ce282f99e768675ca2d2c2a706453eef747
Juan Berta <42556410+JuanBerta@users.noreply.github.com>
2024-06-23 20:55:59 -0300

    Add files via upload
    This now makes that "icons" folder is exported and that an icon for the program is set

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from cx_Freeze import setup, Executable
 build_exe_options = {
     "excludes": ["unittest"],
     "zip_include_packages": ["encodings", "PySide6", "shiboken6"],
+    "include_files": ["icons/"],
 }
 
 setup(
@@ -11,5 +12,7 @@ setup(
     version="0.1",
     description="My GUI application!",
     options={"build_exe": build_exe_options},
-    executables=[Executable("Word-like-notepad.py", base="gui")],
+    executables=[
+        Executable("Word-like-notepad.py", base="gui", icon="./icons/program_icon.ico")
+    ],
 )
